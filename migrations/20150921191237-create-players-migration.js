7a88de6d5391f3543b96dbb33a4b477f2cc90283
Tim Graboski <tim.graboski@aggregateiq.com>
2015-09-24 09:22:02 -0700

    Trying to get connected_at and last_ping to have default current_timestamp, but sequelize isn't letting me easily.

--- migrations/20150921191237-create-players-migration.js
+++ migrations/20150921191237-create-players-migration.js
@@ -1,57 +1,64 @@
 'use strict';
 
 module.exports = {
-  up: function (migration, DataTypes) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+	up: function (migration, DataTypes) {
+		/*
+			Add altering commands here.
+			Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-    migration.createTable('players',
-	{
-	  id: {
-	    type: DataTypes.INTEGER.UNSIGNED,
-	    primaryKey: true,
-	    autoIncrement: true
-	  },
-	  ip: {
-	    type: DataTypes.INTEGER.UNSIGNED,
-	  },
-	  initials: {
-	    type: DataTypes.STRING,
-	    length: 3
-	  },
-	  team: {
-	    type: DataTypes.STRING,
-	    length: 3
-	  },
-	  connected_at: {
-	    type: DataTypes.DATE
-	  },
-	  last_ping: {
-	    type: DataTypes.DATE
-	  }
+			Example:
+			return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		*/
+		migration.createTable('players',
+			{
+				id: {
+					type: DataTypes.INTEGER.UNSIGNED,
+					primaryKey: true,
+					autoIncrement: true,
+					allowNull: false
+				},
+				ip: {
+					type: DataTypes.INTEGER.UNSIGNED,
+					allowNull: false
+				},
+				initials: {
+					type: DataTypes.CHAR,
+					allowNull: false,
+					length: 3
+				},
+				team: {
+					type: DataTypes.CHAR,
+					length: 3
+				},
+				connected_at: {
+					type: DataTypes.DATE,
+					allowNull: false,
+					defaultValue: DataTypes.NOW
+				},
+				last_ping: {
+					type: DataTypes.DATE,
+					allowNull: false,
+					defaultValue: DataTypes.NOW
+				}
+			}
+		);
+
+		migration.addIndex('players',
+			['initials', 'team', 'connected_at', 'last_ping'],
+			{
+				indexName: 'intial_team_connection_index'
+			}
+		);
+	},
+
+	down: function (migration, DataTypes) {
+		/*
+			Add reverting commands here.
+			Return a promise to correctly handle asynchronicity.
+
+			Example:
+			return queryInterface.dropTable('users');
+		*/
+		migration.dropTable('players');
 	}
-    );
-
-    migration.addIndex('players',
-	['initials', 'team', 'connected_at', 'last_ping'],
-	{
-	  indexName: 'intial_team_connection_index'
-	}
-    );
-  },
-
-  down: function (migration, DataTypes) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-    migration.dropTable('players');
-  }
 };
