d68b75cd4beb4744d71f89e6704b3214a3f77e2c
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2015-10-03 16:14:00 -0700

    Add chat testing for team chatroom

--- test/chat_test.js
+++ test/chat_test.js
@@ -33,9 +33,34 @@ describe('Chat Module', function() {
 				done();
 			});
 
-			client.on('chat.enabled', function() {
+			client.on('connect', function() {
 				client.emit('set_initials', 'TST');
 				client.emit('message.global', message);
+			});
+		});
+
+		it('send team message to the right team chatroom', function(done) {
+			var client = io.connect(socket_config.url, socket_config.options);
+			var message = {
+				from: {
+					initials: 'unk',
+					team: 'BOB'
+				},
+				text: 'Hello'
+			};
+
+			client.on('message.team', function(data) {
+				assert.equal(data.from.initials, message.from.initials);
+				assert.equal(data.from.text, message.from.text);
+				client.disconnect();
+				done();
+			});
+
+			client.on('connect', function() {
+				client.on('team.joined', function() {
+					client.emit('message.team', message);
+				});
+				client.emit('set_team', 'BOB');
 			});
 		});
 	});
