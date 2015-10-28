fe882d41d0d31e34edd1c72728644152baa7ef59
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2015-10-27 21:44:49 -0700

    Switch to using player model from ORM
    We're not using the player controller as the model object any more. Now
    we're using the ORM object. Should be able to slowly move toward saving
    to DB only when the user disconnect.

--- models/player.js
+++ models/player.js
@@ -64,7 +64,7 @@ module.exports = function(sequelize, Sequelize) {
 		 * Virtual fields are fields that do not map to the DB
 		 */
 		effects: {
-			type: Sequelize.VIRTUAL,
+			type: new Sequelize.VIRTUAL(Sequelize.ARRAY),
 			validate: {
 				isArray: true
 			}
@@ -74,31 +74,85 @@ module.exports = function(sequelize, Sequelize) {
 		},
 		socket: {
 			type: Sequelize.VIRTUAL
+		},
+		last_tick: {
+			type: Sequelize.VIRTUAL
 		}
-	}, {
+	},
+
+	{
 		tableName: 'players',
 		timestamps: false,
+
 		instanceMethods: {
-			ping: function() {
-				this.updateEffects();
-				this.applyEffects();
-			},
-			updateEffects: function() {
-				var activeEffects = [];
-				for(var i = this.effects.legnth; i >= 0; i--) {
-					var effect = this.effects[i];
-					if (!effect.expired()){
-						activeEffects.push(effect);
-					}
+			// update the player: calculate their attacks, update score based on tick length, and
+			// update the client's score.
+			tick: function(now) {
+				if (!this.last_tick){
+					this.last_tick = now;
+					return;
 				}
 
-				this.effects = activeEffects;
+				this.last_tick = now;
+				var delay_seconds = now - this.last_tick;
+				this.score += delay_seconds * this.spm;
+
+				this.updateEffects();
+				this.applyEffects();
+				this.send_score();
 			},
+
+			// Takes an attack. If it's not for this team, brush it off. Otherwise update your score
+			// speed.
+			defend: function(effect){
+				Logger.debug('Player.defend');
+				if (effect.team !== this.team || effect.expired()){
+					return;
+				}
+
+				this.effects.push(effect);
+				this.tick(Timer.now());
+			},
+
+			// Immediate deduction to the player's score. a value of 60 = one spin. Highers means
+			// more score subtracted.
+			penalize: function(penalty_seconds){
+				Logger.debug('Player.penalize');
+				this.score.seconds = Math.max(0, this.score.seconds - penalty_seconds);
+				this.send_score();
+			},
+
+			// update the client about their score. See above for the score format
+			send_score: function(){
+				Logger.debug('Player.send_score');
+				this.socket.emit('score', this.score);
+			},
+
+			/**
+			 * goes through all effects and attacks applied to this player and calculates an updated
+			 * score speed (aka spins per minute: a value of 1 mean 1 spin per minute. Less means
+			 * slower.)
+			 */
+			updateEffects: function() {
+				if (this.effects.length > 0) {
+					var activeEffects = [];
+					for(var i = this.effects.length; i >= 0; i--) {
+						var effect = this.effects[i];
+						if (!effect.expired()){
+							activeEffects.push(effect);
+						}
+					}
+
+					this.effects = activeEffects;
+				}
+			},
+
 			applyEffects: function() {
 				if (!this.effects.length) {
 					this.spm = 1;
 				}
 			},
+
 			/**
 			 * since we're still tracking the score in the player controller, we
 			 * need to pass that into the model for now.
