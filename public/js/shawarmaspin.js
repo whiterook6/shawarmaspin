9569bd40e14391b86704d9901e8688309ad347af
whiterook6 <whiterook6@gmail.com>
2015-11-07 15:16:23 -0800

    Had to remove the services so we could control when io.connect() was called.

--- public/js/shawarmaspin.js
+++ public/js/shawarmaspin.js
@@ -8,7 +8,7 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			.when('/:team',  { templateUrl: 'views/spin.html',   controller: 'ShawarmaController', controllerAs: 'shawarma_ctrl' });
 	}])
 
-	.controller('ShawarmaController', ['$interval', '$routeParams', 'Socket', 'Team', 'Player', 'Chat', function($interval, $routeParams, Socket, Team, Player, Chat) {
+	.controller('ShawarmaController', ['$interval', '$routeParams', function($interval, $routeParams) {
 		function print_score(score){
 			if (score < 10){
 				return score.toFixed(3);
@@ -25,8 +25,8 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			return (new Date() / 1000.0);
 		}
 
-		var shawarma_ctrl = this;
-		angular.extend(shawarma_ctrl, {
+		var ctrl = this;
+		angular.extend(ctrl, {
 			boards: {
 				online: [],
 				high_scores: [],
@@ -83,113 +83,125 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 
 		Modernizr.on('videoautoplay', function(result){
 			if (result){
-				shawarma_ctrl.spinning = true;
+				ctrl.spinning = true;
 			} else {
-				shawarma_ctrl.spin = function(){
-					if (!shawarma_ctrl.spinning){
-						shawarma_ctrl.spinning = true;
+				ctrl.spin = function(){
+					if (!ctrl.spinning){
+						ctrl.spinning = true;
 						document.getElementById("bgvid").play();
 					}
 				};
 			}
 		});
 
-		shawarma_ctrl.set_name = function(){
-			if (shawarma_ctrl.display.initials == Player.initials){
+		ctrl.set_name = function(){
+			if (ctrl.display.initials == ctrl.player.initials){
 				return;
 			}
 
-			shawarma_ctrl.display.initials = Player.set_name(shawarma_ctrl.display.initials);
-			shawarma_ctrl.player.initals = shawarma_ctrl.display.initials;
+			ctrl.socket.emit('set_initials', ctrl.display.initials);
+			ctrl.player.initals = ctrl.display.initials;
 		};
-		shawarma_ctrl.reset_name = function(){
-			shawarma_ctrl.display.initials = Player.initials;
+		ctrl.reset_name = function(){
+			ctrl.display.initials = Player.initials;
 		};
 
-		shawarma_ctrl.set_team = function(){
-			if (shawarma_ctrl.display.team == Team.name){
+		ctrl.set_team = function(){
+			if (ctrl.display.team == ctrl.player.name){
 				return;
 			}
 
-			shawarma_ctrl.display.team = Team.set_name(shawarma_ctrl.display.team);
-			shawarma_ctrl.player.team = shawarma_ctrl.display.team;
+			ctrl.socket.emit('set_team', ctrl.display.team);
+			ctrl.player.team = ctrl.display.team;
 
-			shawarma_ctrl.messages_team = [];
+			ctrl.messages_team = [];
 		};
 
-		shawarma_ctrl.reset_team = function(){
-			shawarma_ctrl.display.team = Team.name;
+		ctrl.reset_team = function(){
+			ctrl.display.team = ctrl.display.team;
 		};
 
-		shawarma_ctrl.update_name = function(){
-			shawarma_ctrl.set_name();
-			shawarma_ctrl.set_team();
+		ctrl.update_name = function(){
+			ctrl.set_name();
+			ctrl.set_team();
 		};
 
-		shawarma_ctrl.needs_name_updated = function(){
-			return ((Player.initials != shawarma_ctrl.display.initials) || (Team.name != shawarma_ctrl.display.team));
+		ctrl.needs_name_updated = function(){
+			var check = ((ctrl.display.initials != ctrl.player.initials) || (ctrl.display.team != ctrl.player.team));
+			return check;
 		};
 
-		shawarma_ctrl.send_message_global = function(){
-			if (shawarma_ctrl.new_message_global){
-				var message = new Chat.Message(shawarma_ctrl.new_message_global);
-				message.send_to_global();
+		ctrl.send_message_global = function(){
+			if (ctrl.new_message_global){
+				var message = {
+					from: {
+						initials: ctrl.player.initials,
+						team: ctrl.player.team
+					}, text: ctrl.new_message_global
+				};
 
-				shawarma_ctrl.messages_global.push(message);
-				shawarma_ctrl.new_message_global = null;
+				ctrl.socket.emit('message.global', message);
+				ctrl.messages_global.push(message);
+				ctrl.new_message_global = null;
 			}
 		};
 
-		shawarma_ctrl.send_message_team = function(){
-			if (shawarma_ctrl.new_message_team){
-				var message = new Chat.Message(shawarma_ctrl.new_message_team);
-				message.send_to_team();
+		ctrl.send_message_team = function(){
+			if (ctrl.new_message_team){
+				var message = {
+					from: {
+						initials: ctrl.player.initials,
+						team: ctrl.player.team
+					}, text: ctrl.new_message_team
+				};
 
-				shawarma_ctrl.messages_team.push(message);
-				shawarma_ctrl.new_message_team = null;
+				ctrl.socket.emit('message.team', message);
+				ctrl.messages_team.push(message);
+				ctrl.new_message_team = null;
 			}
 		};
 
-		Socket.io.on('connect', function(){
-			shawarma_ctrl.set_name();
+		ctrl.socket = io.connect();
+		ctrl.socket.on('connect', function(){
+			ctrl.set_name();
 			
-			if ($routeParams.team && !shawarma_ctrl.display.team){
-				shawarma_ctrl.display.team = $routeParams.team;
+			if ($routeParams.team && !ctrl.display.team){
+				ctrl.display.team = $routeParams.team;
 			}
-			shawarma_ctrl.set_team();
+			ctrl.set_team();
 			
-			shawarma_ctrl.player.score_seconds = 0.0;
-			shawarma_ctrl.player.spm = 1;
-			shawarma_ctrl.display.score = 0.0;
-			shawarma_ctrl.connected = true;
+			ctrl.player.score_seconds = 0.0;
+			ctrl.player.spm = 1;
+			ctrl.display.score = 0.0;
+			ctrl.connected = true;
 		});
 
-		Socket.io.on('disconnect', function(data){
-			shawarma_ctrl.connected = false;
+		ctrl.socket.on('disconnect', function(data){
+			ctrl.connected = false;
 		});
 
-		Socket.io.on('online', function(data){
+		ctrl.socket.on('online', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.boards.online = [];
+			ctrl.boards.online = [];
 			for (var i = 0; i < data.length; i++) {
-				shawarma_ctrl.boards.online.push({
+				ctrl.boards.online.push({
 					initials: data[i].initials
 				});
 			}
 		});
 
-		Socket.io.on('high_scores', function(data){
+		ctrl.socket.on('high_scores', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.boards.high_scores = [];
+			ctrl.boards.high_scores = [];
 			for (var i = 0; i < data.length; i++) {
 				var datum = data[i];
-				shawarma_ctrl.boards.high_scores.push({
+				ctrl.boards.high_scores.push({
 					rank: i,
 					initials: datum.initials,
 					score_seconds: datum.score_seconds,
@@ -199,16 +211,16 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			}
 		});
 
-		Socket.io.on('team_high_scores', function(data){
+		ctrl.socket.on('team_high_scores', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.boards.team_scores = [];
+			ctrl.boards.team_scores = [];
 			for (var i = 0; i < data.length; i++) {
 				var datum = data[i];
 
-				shawarma_ctrl.boards.team_scores.push({
+				ctrl.boards.team_scores.push({
 					rank: i,
 					team: datum.team,
 					score_seconds: datum.score_seconds,
@@ -218,15 +230,15 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			}
 		});
 
-		Socket.io.on('team_online', function(data){
+		ctrl.socket.on('team_online', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.boards.team_online = [];
+			ctrl.boards.team_online = [];
 			for (var i = 0; i < data.length; i++) {
 				var datum = data[i];
-				shawarma_ctrl.boards.team_online.push({
+				ctrl.boards.team_online.push({
 					initials: datum.initials,
 					score_seconds: datum.score_seconds,
 					display_score: print_score(datum.score_seconds / 60.0),
@@ -235,12 +247,12 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			}
 		});
 
-		Socket.io.on('message.global', function(data){
+		ctrl.socket.on('message.global', function(data){
 			if (!data){
 				return;
 			}
 
-			var messages = shawarma_ctrl.messages_global;
+			var messages = ctrl.messages_global;
 
 			if (messages.length > 10){
 				messages.shift();
@@ -249,12 +261,12 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			messages.push(data);
 		});
 
-		Socket.io.on('message.team', function(data){
+		ctrl.socket.on('message.team', function(data){
 			if (!data){
 				return;
 			}
 
-			var messages = shawarma_ctrl.messages_team;
+			var messages = ctrl.messages_team;
 
 			if (messages.length > 10){
 				messages.shift();
@@ -263,126 +275,44 @@ angular.module('ShawarmaSpinApp', ['ngRoute'])
 			messages.push(data);
 		});
 
-		Socket.io.on('new_initials', function(data){
+		ctrl.socket.on('new_initials', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.player.initials = data;
-			shawarma_ctrl.reset_name();
+			ctrl.player.initials = data;
+			ctrl.reset_name();
 		});
 
-		Socket.io.on('new_team', function(data){
+		ctrl.socket.on('new_team', function(data){
 			if (!data){
 				return;
 			}
 
-			shawarma_ctrl.player.team = data;
-			shawarma_ctrl.reset_team();
+			ctrl.player.team = data;
+			ctrl.reset_team();
 		});
 
-		shawarma_ctrl.interval = 1.0 / 60.0;
-		shawarma_ctrl.last_tick = now();
+		ctrl.interval = 1.0 / 60.0;
+		ctrl.last_tick = now();
 		$interval(function(){
-			var delay = now() - shawarma_ctrl.last_tick;
-			shawarma_ctrl.last_tick = now();
+			var delay = now() - ctrl.last_tick;
+			ctrl.last_tick = now();
 
-			if (shawarma_ctrl.connected){
-				shawarma_ctrl.boards.tick(delay);
-				shawarma_ctrl.player.score_seconds += delay * shawarma_ctrl.player.spm;
-				shawarma_ctrl.display.score = print_score(shawarma_ctrl.player.score_seconds / 60);
+			if (ctrl.connected){
+				ctrl.boards.tick(delay);
+				ctrl.player.score_seconds += delay * ctrl.player.spm;
+				ctrl.display.score = print_score(ctrl.player.score_seconds / 60);
 			}
 
-		}, shawarma_ctrl.interval * 1000);
+		}, ctrl.interval * 1000);
 
-		Socket.io.on('score', function(data){
-			shawarma_ctrl.player.score_seconds = parseFloat(data.score_seconds);
-			shawarma_ctrl.player.spm = data.spm;
+		ctrl.socket.on('score', function(data){
+			ctrl.player.score_seconds = parseFloat(data.score_seconds);
+			ctrl.player.spm = data.spm;
 
-			shawarma_ctrl.display.score = print_score(shawarma_ctrl.player.score_seconds / 60);
+			ctrl.display.score = print_score(ctrl.player.score_seconds / 60);
 		});
-	}])
-
-	.factory('Socket', [function() {
-		var Socket = {
-			io: io.connect()
-		};
-		return Socket;
-	}])
-
-	.factory('Player', ['Socket', function(Socket) {
-		var Player = {
-			initials: 'unk',
-
-			set_name: function(init) {
-				if (!init){
-					return;
-				}
-
-				init = init.substring(0, 3);
-				if (init != this.initials) {
-					this.initials = init;
-					Socket.io.emit('set_initials', init);
-					return this.initials;
-				}
-				else {
-					return null;
-				}
-			}
-		};
-		return Player;
-	}])
-
-	.factory('Team', ['Socket', function(Socket) {
-		var Team = {
-			name: '',
-			set_name: function(name) {
-				if (name){
-					name = name.substring(0, 3);
-				} else {
-					name = null;
-				}
-
-				if (name !== this.name) {
-					this.name = name;
-					Socket.io.emit('set_team', name);
-					return this.name;
-				}
-			}
-		};
-		return Team;
-	}])
-
-	.factory('Chat', ['Socket', 'Player', 'Team', function(Socket, Player, Team) {
-		var Chat = {
-			messages: {
-				global: [],
-				team: []
-			}
-		};
-			
-		Message = function(text){
-			this.from = {
-				initials: Player.initials,
-				team: Team.name
-			};
-			this.text = text;
-		};
-
-		Message.prototype = {
-			constructor: Message,
-			
-			send_to_team: function(){
-				return Socket.io.emit('message.team', this);
-			},
-
-			send_to_global: function(){
-				return Socket.io.emit('message.global', this);
-			}
-		};
-
-		Chat.Message = Message;
-		return Chat;
 	}]);
 
 	soundManager.setup({
